709155b899fb30896ea16a67ec9742112756b426
Lei Yang <lei@yangl1996.com>
2022-03-07 22:55:02 -0500

    simplify the api

--- game/merkle_tree.go
+++ game/merkle_tree.go
@@ -1,20 +1,8 @@
 package game
 
-type MerkleMountainRange struct {
-	Trees []MerkleTree
-	Sizes []int	// number of elements
-}
-
-func (m *MerkleMountainRange) GetPeaks() []Hash {
-	res := []Hash{}
-	for _, t := range m.Trees {
-		res = append(res, t.GetRoot())
-	}
-	return res
-}
-
 type MerkleTree interface {
-	GetRoot() Hash
+	GetSubtreeSize() int
+	GetRoots() []Hash
     GetChildren(node Hash) []Hash
     GetProof(node Hash) []Hash
 	IsLeaf(node Hash) bool
