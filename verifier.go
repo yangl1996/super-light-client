5153e2183134a4dfe6f99f39fc12b712a7c073e5
Lei Yang <lei@yangl1996.com>
2022-03-26 21:49:09 -0400

    back to back requests

--- verifier.go
+++ verifier.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"net"
 	"time"
+	"sync"
 )
 
 func newVerifier(servers []string, deg int) *game.Verifier {
@@ -38,33 +39,58 @@ func newVerifier(servers []string, deg int) *game.Verifier {
 func verify(args []string) {
 	cmd := flag.NewFlagSet("verify", flag.ExitOnError)
 	deg := cmd.Int("dim", 50, "dimension of the tree")
-	num := cmd.Int("N", 10, "number of back-to-back verifications to perform")
+	num := cmd.Int("N", 10, "number of back-to-back verifications per thread")
+	burst := cmd.Int("p", 1, "number of threads to generate verifications")
 	cmd.Parse(args)
 	servers := cmd.Args()
 	if len(servers) < 2 {
 		log.Fatalln("supply at least 2 servers as command line arguments")
 	}
 
-	v := newVerifier(cmd.Args(), *deg)
+	wg := &sync.WaitGroup{}
+	l := &sync.Mutex{}
+	tot := 0.0
+	cnt := 0
+	totSq := 0.0
+	resCh := make(chan float64, 1000)
+	l.Lock()
+	go func() {
+		defer l.Unlock()
+		for s := range resCh {
+			tot += s
+			totSq += s * s
+			cnt += 1
+		}
+	}()
 
-	durs := []float64{}
 	log.Printf("running verifications")
-	for i := 0; i < *num; i++ {
-		start := time.Now()
-		_, winner := v.Run()
-		dur := float64(time.Since(start).Milliseconds())
-		durs = append(durs, dur)
-		log.Printf("server %v is winner\n", winner)
+	initWg := &sync.WaitGroup{}
+	for node := 0; node < *burst; node++ {
+		wg.Add(1)
+		initWg.Add(1)
+		go func() {
+			v := newVerifier(cmd.Args(), *deg)
+			initWg.Done()
+			initWg.Wait()
+			for i := 0; i < *num; i++ {
+				start := time.Now()
+				_, winner := v.Run()
+				dur := float64(time.Since(start).Milliseconds())
+				resCh <- dur
+				if *burst == 1 {
+					log.Printf("server %v is winner\n", winner)
+				}
+			}
+			wg.Done()
+		}()
 	}
-	avg := 0.0
-	sqavg := 0.0
-	for _, dur := range durs {
-		avg += dur
-		sqavg += dur * dur
-	}
-	avg /= float64(len(durs))
-	sqavg /= float64(len(durs))
+	wg.Wait()
+	close(resCh)
+	l.Lock()
+	avg := tot / float64(cnt)
+	stddev := math.Sqrt(totSq / float64(cnt) - avg * avg)
+	l.Unlock()
 
-	log.Printf("finished %v runs, avg %.2f ms, stddev %.2f ms\n", len(durs), avg, math.Sqrt(sqavg-avg*avg))
+	log.Printf("finished %v runs, avg %.2f ms, stddev %.2f ms\n", cnt, avg, stddev)
 }
 
